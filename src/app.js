7f7e23d51122ecc71179ee0177bc9284948538c6
MohdAnas9721 <mohdanas972175@gmail.com>
2026-02-23 15:39:36 +0530

    My portfolio Anas

--- src/app.js
+++ src/app.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 const routes = require("./routes/index.routes");
 const errorMiddleware = require("./middlewares/error.middleware");
 
 const app = express();
+const frontendDist = path.resolve(__dirname, "../../frontend/dist");
+const frontendIndex = path.join(frontendDist, "index.html");
+const hasFrontendBuild = fs.existsSync(frontendIndex);
+const clientOrigin = process.env.CLIENT_ORIGIN || "http://localhost:5173";
 
-app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: false }));
+app.use(cors({ origin: clientOrigin, credentials: false }));
 app.use(express.json());
 
-// 👇 YE LINE ADD KI
-app.use(express.static("public"));
+if (hasFrontendBuild) {
+  app.use(express.static(frontendDist));
+}
 
-app.get("/", (req, res) => res.send("Portfolio Backend ✅"));
+app.get("/", (req, res) => {
+  if (hasFrontendBuild) {
+    return res.sendFile(frontendIndex);
+  }
+  return res.send("Portfolio Backend API running");
+});
+
 app.use("/api", routes);
 
 app.use(errorMiddleware);
